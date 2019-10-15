80e28d254a4dde960a70ca89c44ea8be7ac02ceb
Alex Lindroos <alex.lindroos@codemate.com>
2019-10-15 15:58:03 +0300

    Changed title

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Fading Dialog'),
     );
   }
 }
